0c90a477f6dfda723decf3e79f7e72656f9b15a0
Eric Lee <ericlee@Daewoos-Mac.local>
2017-04-07 15:16:55 -0600

    Created readme file

--- src/money/ChangeMaker.java
+++ src/money/ChangeMaker.java
@@ -1,9 +1,13 @@
 package money;
+import java.text.DecimalFormat;
 
 public class ChangeMaker {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double price = 1.03;
+		double paid = 1;
+		double change = price - paid;
+		System.out.println(price - paid);
 
 	}
 
