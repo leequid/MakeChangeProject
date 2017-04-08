cf7b07428386dea0f0dc9cd76855ca2c0ebcf657
Eric Lee <ericlee@Daewoos-Mac.local>
2017-04-08 09:35:32 -0600

    2 decimal format

--- src/money/ChangeMaker.java
+++ src/money/ChangeMaker.java
@@ -1,13 +1,19 @@
 package money;
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class ChangeMaker {
 
 	public static void main(String[] args) {
 		double price = 1.03;
 		double paid = 1;
-		double change = price - paid;
-		System.out.println(price - paid);
+		double changes = price - paid;
+		String change;
+		Scanner kb = new Scanner(System.in);
+		DecimalFormat df = new DecimalFormat("#.00"); 
+		change = df.format(changes);
+		System.out.println(change);
+	
 
 	}
 
